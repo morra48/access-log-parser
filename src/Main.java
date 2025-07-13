9d9ea81955e67e0c769de6d9f699dea3ae5d4d32
Andrey Filishchinskiy <andrei.filishinsky@gmail.com>
2025-07-13 19:21:45 +0300

    1. Изменение логики работы класса Main. Добавлен подсчет суммы, разности, произведения и частного двух чисел.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-//        System.out.println("Случайное число от 0 до 1: " + Math.random());
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма:" + (firstNumber + secondNumber));
+        System.out.println("Разность:" + (firstNumber - secondNumber));
+        System.out.println("Произведение:" + (firstNumber * secondNumber));
+        System.out.println("Частное:" + ((double)firstNumber / secondNumber));
     }
 }
